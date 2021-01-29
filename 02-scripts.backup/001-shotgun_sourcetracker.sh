85a312ce3e608ba08e4f295c0f2bcabfac854e14
Irina Velsko <velsko@cdag1-new.cdag.shh.mpg.de>
2021-01-29 11:37:56 +0100

    updated sourcetracker results

--- 02-scripts.backup/001-shotgun_sourcetracker.sh
+++ 02-scripts.backup/001-shotgun_sourcetracker.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-OUTDIR=/projects1/microbiome_calculus/smoking_calculus/04-analysis/sourceTracker/shotgun/full_sources
+OUTDIR=/projects1/microbiome_calculus/smoking_calculus/04-analysis/sourceTracker/shotgun/rarefied_table
 
 # MAPPINGFILE=/projects1/microbiome_calculus/smoking_calculus/00-documentation.backup/source_tracker_mappingfile_new.tsv
 #MAPPINGFILE=/projects1/microbiome_calculus/dental_arcade/05-Files.backup/modern_calculus_mappingfile.tsv
 
 sbatch \
-	-c 2 \
-	--mem=8000 \
+	-c 12 \
+	--mem=24000 \
 	-o ~/slurm_logs/slurm.%j.out \
 	-e ~/slurm_logs/slurm.%j.err \
 	--partition=long \
@@ -18,9 +18,10 @@ sbatch \
 	-J "Sourcetracker" \
 	--wrap="Rscript \
 	/projects1/users/velsko/bin/sourcetracker-1.0.1/sourcetracker_for_qiime.r \
-	-i /projects1/microbiome_calculus/smoking_calculus/04-analysis/sourceTracker/shotgun/shotgun_sourcetracker_table_noblanks.txt \
+	-i /projects1/microbiome_calculus/smoking_calculus/04-analysis/sourceTracker/shotgun/shotgun_sourcetracker_table_noblanks_rarefied.txt \
 	-m /projects1/microbiome_calculus/smoking_calculus/00-documentation.backup/source_tracker_mappingfile.tsv \
-	-o "$OUTDIR"/shotgun_sourcetracker_table_log_"$(date +"%Y%m%d")" \
-	-r 5000 \
+	-o "$OUTDIR"/shotgun_sourcetracker_table_rarefied_full_sources_log_"$(date +"%Y%m%d")" \
+	--train_rarefaction 10000 \
+	-r 51500 \
 	-v"
 
--- 02-scripts.backup/001-shotgun_sourcetracker_reduced_source.sh
+++ 02-scripts.backup/001-shotgun_sourcetracker_reduced_source.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-OUTDIR=/projects1/microbiome_calculus/smoking_calculus/04-analysis/sourceTracker/shotgun/reduced_sources
+OUTDIR=/projects1/microbiome_calculus/smoking_calculus/04-analysis/sourceTracker/shotgun/rarefied_table
 
 sbatch \
-	-c 2 \
-	--mem=8000 \
+	-c 12 \
+	--mem=24000 \
 	-o ~/slurm_logs/slurm.%j.out \
 	-e ~/slurm_logs/slurm.%j.err \
 	--partition=long \
@@ -15,9 +15,10 @@ sbatch \
 	-J "Sourcetracker" \
 	--wrap="Rscript \
 	/projects1/users/velsko/bin/sourcetracker-1.0.1/sourcetracker_for_qiime.r \
-	-i /projects1/microbiome_calculus/smoking_calculus/04-analysis/sourceTracker/shotgun/shotgun_sourcetracker_table_noblanks.txt \
+	-i /projects1/microbiome_calculus/smoking_calculus/04-analysis/sourceTracker/shotgun/shotgun_sourcetracker_table_noblanks_rarefied.txt \
 	-m /projects1/microbiome_calculus/smoking_calculus/00-documentation.backup/source_tracker_mappingfile_new.tsv \
-	-o "$OUTDIR"/shotgun_sourcetracker_table_log_"$(date +"%Y%m%d")" \
-	-r 5000 \
+	-o "$OUTDIR"/shotgun_sourcetracker_table_rarefied_reduced_sources_log_"$(date +"%Y%m%d")" \
+	--train_rarefaction 10000 \
+	-r 51500 \
 	-v"
 
